2db16cd8a9314d8099e1fdb0df7bf0fb4e695c1e
kamilam1987 <kamilam1987@gmail.com>
2018-03-31 14:29:40 +0100

    added moving car

---
+++ CarGame/Assets/Scripts/RoadInfiniteScrolling.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Author: Kamila Michel
+//Source code based on : https://www.youtube.com/watch?v=0ZZlMIZyPUk
+
+public class RoadInfiniteScrolling : MonoBehaviour {
+
+    //Declares variables
+    public float scrollSpeed;
+    private Vector2 offset;
+
+    private void Update()
+    {
+        offset = new Vector2(0, Time.time * scrollSpeed); //Time and speed
+        GetComponent<Renderer>().material.mainTextureOffset = offset;//Gets component from object which script is assigned to, assigne offset to object texture
+    }//End of Update method
+}//End of RoadInfiniteScrolling class
