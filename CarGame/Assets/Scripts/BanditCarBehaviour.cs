1e1441cf79f760456f38b87e3b759a5097aeeee4
kamilam1987 <kamilam1987@gmail.com>
2018-04-02 22:59:48 +0100

    added bandit car

---
+++ CarGame/Assets/Scripts/BanditCarBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Author: Kamila Michel
+//Source code adapted from: https://www.youtube.com/watch?v=42LU2PO9BAY
+
+public class BanditCarBehaviour : MonoBehaviour {
+    //Declare variables
+    public GameObject bomb;//Bobm game object
+    public int bombAmount;//Amount of bombs
+    public int banditCarVarticalSpeed;
+    public int banditCarHorizontalSpeed;
+    public float bombDelay;//Bombs delay
+
+    private float Delay;//Deley amount
+    private GameObject playerCar;//Object car player
+    private Vector3 banditCarPos;//Position of a bandits car
+
+    void Start()
+    {
+        playerCar = GameObject.FindWithTag("Player");//Find car with game player
+        Delay = bombDelay;//Assigneddelay to bomb delay
+    }//End of Start method
+
+    void Update()
+    {
+        if(playerCar == null)//If didn't find a player car
+        {
+            playerCar = GameObject.FindWithTag("Player");//Looks for object with player tag 
+        }//End of if
+         else
+        {
+            if (gameObject.transform.position.y > 2.65f)
+            {
+                this.gameObject.transform.Translate(new Vector3(0, -1, 0) * banditCarVarticalSpeed * Time.deltaTime);//Speed of driving in and out
+            }//End of if
+            else if (bombAmount <= 0) {//No more bombs
+                this.gameObject.transform.Translate(new Vector3(0, 1, 0) * banditCarVarticalSpeed * Time.deltaTime);//Bandits car disappear 
+                if (gameObject.transform.position.y > 7.48f) {//If on the position
+                    Destroy(this.gameObject);
+                }//End of if
+            }//End of else if
+            else
+            {
+                banditCarPos = Vector3.Lerp(transform.position, playerCar.transform.position, Time.fixedDeltaTime * banditCarHorizontalSpeed);//Follows the player car
+                transform.position = new Vector3(banditCarPos.x, transform.position.y, 0);//How fast will be moving
+
+                //Check if delay accept to drop a bomb
+                Delay -= Time.deltaTime;
+                if (Delay <= 0 && bombAmount > 0) //If delay 0 or and any bombs left
+                {
+                    Delay = bombDelay; //Assignrd bomb delay
+                    bombAmount--;
+                    Instantiate(bomb, transform.position, Quaternion.identity);//Drops bomb
+                }//End of if
+            }
+        }//End of else
+    }//End of Update method
+
+
+}//End of BanditCarBehaviour class
